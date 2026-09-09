24f34065baecb0576f7ee0190312125ec14f7921
epkisiel <uT_4002@zsp3zamosc.pl>
2026-09-09 12:19:37 +0200

    Add files via upload

---
+++ 11_2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Zadanie
+{
+    public class Ksiazka
+    {
+        private string tytul;
+        private string autor;
+        private int liczbaStron;
+        public string Tytul { get { return tytul; } set { tytul = value; } }
+        public string Autor { get { return autor; } set { autor = value; } }
+        public int LiczbaStron { get { return liczbaStron; } set { if (value > 0) { liczbaStron = value; } else { liczbaStron = 1; } } }
+
+        public Ksiazka(string tytul, string autor, int liczbaStron)
+        {
+            Tytul= tytul;
+            Autor= autor;
+            LiczbaStron= liczbaStron;
+        }
+
+        public void Opisz()
+        {
+            Console.WriteLine($"\"{tytul}\" {autor}, liczba stron: {liczbaStron}");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        var ksiazka = new Ksiazka("Tytuł książki", "Autor Książki", 300);
+        ksiazka.Opisz();
+    }
+}
