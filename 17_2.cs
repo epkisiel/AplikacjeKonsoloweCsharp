77c9d36738e690da6a4ad74edc68285a9863b41a
epkisiel <uT_4002@zsp3zamosc.pl>
2026-09-15 10:17:15 +0200

    Add files via upload

---
+++ 17_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Zadanie
+{
+    static int PobierzRokUrodzenia(string pesel)
+    {
+        string cyfryRoku = pesel[0].ToString()+pesel[1].ToString();
+        string cyfryMiesiaca=pesel[2].ToString()+pesel[3].ToString();
+
+        if (int.Parse(cyfryMiesiaca)>=1&&int.Parse(cyfryMiesiaca)<=12)
+        {
+            return 1900 + int.Parse(cyfryRoku);
+        }
+        else
+        {
+            return 2000 + int.Parse(cyfryRoku);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        
+    }
+} 
